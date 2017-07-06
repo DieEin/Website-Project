78213941c03395e67771e97ab7baa3132b43cdfc
DieEin <savageshadow@abv.bg>
2017-07-06 10:26:08 +0300

    initial commit #2

---
+++ WebsiteProject/WebsiteProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebsiteProject.Startup))]
+namespace WebsiteProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
